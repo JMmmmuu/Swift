f9ea635cb5948b18eb9477922abf3f84ab492ffa
JMmmmuu <mkyseok1550@gmail.com>
2018-08-16 15:21:34 +0900

    todo-TableViewController

--- followUps/Todos/TodosTableViewController.swift
+++ followUps/Todos/TodosTableViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class TodosTableViewController: UITableViewController {
 
+    // MARK:- Properties
+    // MARK: Privates
+    /// todo 목록 - dummy data
+    private var todos: [Todo] = [Todo(title: "test1", due: Date(), memo: "memo1", shouldNotify: true, id: "1"),
+                                 Todo(title: "test2", due: Date(), memo: "memo2", shouldNotify: true, id: "2"),
+                                 Todo(title: "test3", due: Date(), memo: "memo3", shouldNotify: true, id: "3")]
+    
+    /// 셀에 표시할 날짜를 포맷하기 위한 Date Formatter
+    private let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateStyle = DateFormatter.Style.medium
+        formatter.timeStyle = DateFormatter.Style.short
+        return formatter
+    } ()
+    
+    // MARK:- Methods
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,8 +34,18 @@ class TodosTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        // UIViewController에서 제공하는 기본 수정버튼
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 화면이 보일 때마다 todo 목록을 새로고침
+        self.todos = Todo.all
+        self.tableView.reloadSections(IndexSet(integer: 0), with: UITableViewRowAnimation.automatic)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -26,27 +53,36 @@ class TodosTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+    /// 테이블뷰의 섹션 수 (defaul 1)
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
+    /// 테이블뷰의 섹션 별 로우수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.todos.count
     }
-
-    /*
+    
+    /// 인덱스에 해당하는 cell 반환
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // 스토리보드에 구현해 둔 셀을 재사용 큐에서 꺼내옴
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        guard indexPath.row < self.todos.count else { return cell }
 
-        // Configure the cell...
+        let todo: Todo = self.todos[indexPath.row]
+        
+        // 셀에 내용 설정
+        cell.textLabel?.text = todo.title
+        cell.detailTextLabel?.text = self.dateFormatter.string(from: todo.due)
 
         return cell
     }
-    */
-
+ 
+ 
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
