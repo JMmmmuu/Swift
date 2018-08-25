7bb77ecd617065cafece76bc51b9427e3bf3fdb5
Yuseok <mkyseok1550@gmail.com>
2018-08-26 00:17:23 +0900

    Swift-add AppBrewery Projects

---
+++ Udemy/AppBrewery/Projects/1_I-Am-Rich/1_I-Am-RichTests/__I_Am_RichTests.swift
@@ -0,0 +1,36 @@
+//
+//  __I_Am_RichTests.swift
+//  1_I-Am-RichTests
+//
+//  Created by Yuseok on 2018. 8. 25..
+//  Copyright © 2018년 Yuseok. All rights reserved.
+//
+
+import XCTest
+@testable import __I_Am_Rich
+
+class __I_Am_RichTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
