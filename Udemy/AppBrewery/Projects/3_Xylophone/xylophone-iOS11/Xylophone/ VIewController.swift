d3a556140b97315deb7ba9c0a5e96fa37e4722f9
Yuseok <mkyseok1550@gmail.com>
2018-09-04 00:13:12 +0900

    Add note1

--- Udemy/AppBrewery/Projects/3_Xylophone/xylophone-iOS11/Xylophone/ VIewController.swift
+++ Udemy/AppBrewery/Projects/3_Xylophone/xylophone-iOS11/Xylophone/ VIewController.swift
@@ -11,7 +11,7 @@ import AVFoundation
 
 class ViewController: UIViewController{
 
-    var player: AVAudioPlayer?
+    //var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,17 +21,24 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
-        
-        do {
-            player = try AVAudioPlayer(contentsOf: url)
-            guard let player = player else { return }
-            
-            player.prepareToPlay()
-            player.play()
-        } catch let error as Error {
-            print(error.localizedDescription)
+//        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+//
+//        do {
+//            player = try AVAudioPlayer(contentsOf: url)
+//            guard let player = player else { return }
+//
+//            player.prepareToPlay()
+//            player.play()
+//        } catch let error as Error {
+//            print(error.localizedDescription)
+//        }
+        if let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav") {
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            // Play
+            AudioServicesPlaySystemSound(mySound)
         }
+        print("\(sender.tag)")
     }
     
   
