3c0914dd5d5b8b5495f2c4dd71635c1a023fda33
JMmmmuu <mkyseok1550@gmail.com>
2019-08-02 09:59:47 +0900

    add property list

--- Udemy/AppBrewery/Projects/8_Clima-iOS12-master/Clima/WeatherViewController.swift
+++ Udemy/AppBrewery/Projects/8_Clima-iOS12-master/Clima/WeatherViewController.swift
@@ -7,9 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -18,6 +18,7 @@ class WeatherViewController: UIViewController {
     
 
     //TODO: Declare instance variables here
+    let locationManager = CLLocationManager()
     
 
     
@@ -32,7 +33,9 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
         
         
     }
