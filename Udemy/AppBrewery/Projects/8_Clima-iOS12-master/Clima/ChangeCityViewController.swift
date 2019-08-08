95d489b20e9b59bcdeb879fd391b61d2fe7ad4f0
JMmmmuu <mkyseok1550@gmail.com>
2019-08-08 12:32:08 +0900

    add protocol & property

--- Udemy/AppBrewery/Projects/8_Clima-iOS12-master/Clima/ChangeCityViewController.swift
+++ Udemy/AppBrewery/Projects/8_Clima-iOS12-master/Clima/ChangeCityViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 
 //Write the protocol declaration here:
+protocol ChangeCityDelegate {
+    func userEnteredANewCityName (city: String)
+}
 
 
 
 class ChangeCityViewController: UIViewController {
     
     //Declare the delegate variable here:
+    var delegate: ChangeCityDelegate?
+    
+
 
     
     //This is the pre-linked IBOutlets to the text field:
@@ -24,17 +30,14 @@ class ChangeCityViewController: UIViewController {
     
     //This is the IBAction that gets called when the user taps on the "Get Weather" button:
     @IBAction func getWeatherPressed(_ sender: AnyObject) {
-        
-        
-        
         //1 Get the city name the user entered in the text field
-        
+        let cityName = changeCityTextField.text!
         
         //2 If we have a delegate set, call the method userEnteredANewCityName
-        
+        delegate?.userEnteredANewCityName(city: cityName)
         
         //3 dismiss the Change City View Controller to go back to the WeatherViewController
-        
+        self.dismiss(animated: true, completion: nil)
         
     }
     
