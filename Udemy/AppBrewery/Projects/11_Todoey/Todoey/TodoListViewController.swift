d7e5f6bd8edba3f354d143f7a21d6ebbb12353cb
JMmmmuu <mkyseok1550@gmail.com>
2019-08-14 14:32:54 +0900

    initiate project & add tableview

---
+++ Udemy/AppBrewery/Projects/11_Todoey/Todoey/TodoListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Yuseok on 14/08/2019.
+//  Copyright © 2019 Yuseok. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Egggos", "Destory Demongorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+
+}
+
