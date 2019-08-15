335f938dbd74470c521b38a8e844dedddbdfa5e3
JMmmmuu <mkyseok1550@gmail.com>
2019-08-15 09:49:32 +0900

    userDefaults to save persistant data

--- Udemy/AppBrewery/Projects/11_Todoey/Todoey/TodoListViewController.swift
+++ Udemy/AppBrewery/Projects/11_Todoey/Todoey/TodoListViewController.swift
@@ -11,10 +11,14 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Buy Egggos", "Destory Demongorgon"]
+    var defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
         
     }
 
@@ -55,6 +59,7 @@ class TodoListViewController: UITableViewController {
             
             if let text: String = textField.text {
                 self.itemArray.append(text)
+                self.defaults.set(self.itemArray, forKey: "TodoListArray")
                 self.tableView.reloadData()
             } else {
             }
