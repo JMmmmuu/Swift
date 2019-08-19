9af8c836c35cc52c473f3da562df19e1dd523611
JMmmmuu <mkyseok1550@gmail.com>
2019-08-19 12:45:11 +0900

    Category page

--- Udemy/AppBrewery/Projects/11_Todoey/Todoey/Controllers/CategoryViewController.swift
+++ Udemy/AppBrewery/Projects/11_Todoey/Todoey/Controllers/CategoryViewController.swift
@@ -7,18 +7,83 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
 
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-    }
-
-
-
-    @IBAction func barButtonPressed(_ sender: UIBarButtonItem) {
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        loadItems(with: request)
     }
     
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        var textField = UITextField()
+        
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            if let text: String = textField.text {
+                
+                let newCategory = Category(context: self.context)
+                newCategory.name = text
+                self.categories.append(newCategory)
+                
+                self.saveItems()
+            }
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+
+    
+    // MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categories[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        return cell
+    }
+    
+    
+    // MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    // MARK: - Data Manipulation Methods
+    func saveItems() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context: \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<Category>) {
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+    }
 }
--- Udemy/AppBrewery/Projects/11_Todoey/Todoey/Controllers/TodoListViewController.swift
+++ Udemy/AppBrewery/Projects/11_Todoey/Todoey/Controllers/TodoListViewController.swift
@@ -11,7 +11,7 @@ import CoreData
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = [Item]() //["Find Mike", "Buy Egggos", "Destory Demongorgon", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var itemArray = [Item]()
     //var defaults = UserDefaults.standard
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
