b0ac4c77240341a7cc9e0c56cd72856e3794dcdb
JMmmmuu <mkyseok1550@gmail.com>
2019-08-31 16:04:13 +0900

    label the detected flower

---
+++ Udemy/AppBrewery/Projects/15_WhatFlower/WhatFlower/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Yuseok on 31/08/2019.
+//  Copyright © 2019 Yuseok. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+        //imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageView.image = userPickedImage
+
+            guard let convertedCIImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage into CIImage")
+            }
+            
+            detect(image: convertedCIImage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            self.navigationItem.title = classification?.identifier.capitalized
+        }
+
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+
+}
+
