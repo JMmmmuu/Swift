cb3fdb63acf5021a9d330b2c06a2b44bbba13aa6
JMmmmuu <mkyseok1550@gmail.com>
2019-09-02 11:21:24 +0900

    ruler project initiate

---
+++ Udemy/AppBrewery/Projects/18_AR Ruler/AR Ruler/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  AR Ruler
+//
+//  Created by Yuseok on 02/09/2019.
+//  Copyright © 2019 Yuseok. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            let hitTestResult = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResult.first {
+                addDot(at: hitResult)
+            }
+        }
+        
+    }
+    
+    
+    func addDot(at hitResult: ARHitTestResult) {
+        let dotGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        dotGeometry.materials = [material]
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        
+        let pos = hitResult.worldTransform.columns.3
+        dotNode.position = SCNVector3(pos.x, pos.y, pos.z)
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+    }
+    
+}
