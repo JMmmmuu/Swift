f7846b80b58b88840b945c6062a389583519d63c
JMmmmuu <mkyseok1550@gmail.com>
2019-09-03 14:55:25 +0900

    play video

--- Udemy/AppBrewery/Projects/MagicPaper/MagicPaper/ViewController.swift
+++ Udemy/AppBrewery/Projects/MagicPaper/MagicPaper/ViewController.swift
@@ -56,8 +56,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let node = SCNNode()
         
         if let imageAnchor = anchor as? ARImageAnchor {
+            let videoNode = SKVideoNode(fileNamed: "Harrypotter.mp4")
+            videoNode.play()
+            
+            let videoScene = SKScene(size: CGSize(width: 1280, height: 720))
+            videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+            videoNode.yScale = -1
+            
+            videoScene.addChild(videoNode)
+            
             let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
-            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            plane.firstMaterial?.diffuse.contents = videoScene
             
             let planeNode = SCNNode(geometry: plane)
             planeNode.eulerAngles.x = -.pi / 2
